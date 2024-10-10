31df3cfb571a84e1a089f38632ef7c3e798fb196
Aj Broman <ajbroman@gmail.com>
2024-10-10 13:21:21 -0400

    Add first checkpoint, C to F conversion

---
+++ Checkpoint1.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+/*
+ Objectives of this Checkpoint:
+ 
+ 1. Create a constant holding any temperature in Celcius.
+ 2. Convert it to Fahrenheit by multiplying by 9, dividing by 5, then adding 32.
+ 3. Print the result for the user, showing both the Celsius and Fahrenheit values.
+ 
+ */
+
+let celcius = 32
+let fahrenheit = (celcius * 9) / 5 + 32
+
+print("The temperature in Celcius is: \(celcius), which converts to \(fahrenheit) degrees Fahrenheit.")
+
+
+
