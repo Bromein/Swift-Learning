0ad9f4637daccbf993066dc0e5d6a8e1fca38c8c
Aj Broman <ajbroman@gmail.com>
2024-10-14 16:14:02 -0400

    Small summary added

---
+++ Complex Data.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+var greeting = "Hello, playground"
+/*
+ - Arrays must contain one data type, and can hold many values -- are read with indices
+ - Dictionaries contain key pairs, can search via keys
+ - Sets store many values, we dont choose the order
+ - Enums create our own types, to specify acceptable values
+ - By default, Swift utilizes type inference. We can however utilize type annotation to specify what we would like a variable/constant to be
+ 
+ Looks like this was just a summary of the previous sections! Thanks for checking it out. 
+ */
